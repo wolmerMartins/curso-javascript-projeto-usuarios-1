e17120557185bcc8bc2f9daaaca82a6a4b5167b2
Wolmer Martins <wolmermartins@hotmail.com>
2019-02-22 23:32:13 -0300

    Function for fill form to edit added

--- _controllers/UserController.js
+++ _controllers/UserController.js
@@ -111,7 +111,27 @@ class UserController {
             </td>
         `;
         tr.querySelector(".btn-edit").addEventListener("click", e => {
-            console.log(JSON.parse(tr.dataset.user));
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+            for (let name in json) {
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+                if (field) {
+                    switch(field.type) {
+                        case "file":
+                            continue;
+                            break;
+                        case "radio":
+                            field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+                            field.checked = true;
+                            break;
+                        case "checkbox":
+                            field.checked = json[name];
+                            break;
+                        default:
+                            field.value = json[name];
+                    }
+                }
+            }
             this.switchBoxes("none", "block");
         });
 
